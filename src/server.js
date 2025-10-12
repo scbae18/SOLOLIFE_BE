c802cbc1d066b642e67809a4d52a989512304f42
scbae18 <scbae18@gmail.com>
2025-10-12 16:10:21 +0900

    fix:weather 수정

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import app from './app.js';
+import dns from 'dns';
 
+dns.setDefaultResultOrder('ipv4first');
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`API listening on :${PORT}`));
